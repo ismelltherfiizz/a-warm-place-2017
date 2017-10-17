093e722516ef9181d68149ea41af383f7227ced1
mrselfdestructt <shkolniy@dlit.dp.ua>
2017-10-18 01:52:21 +0300

    added lab1.cpp,lab2.cpp,lab3.cpp changed README.md starting line

---
+++ lab2.cpp
@@ -0,0 +1,85 @@
+﻿// lab2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char choose;
+	double x, y, a, b, h, d;
+	cout << "Choose the variant 'a' or 'b':" << endl;
+	cin >> choose;
+	while (choose != 'a' && choose != 'b')
+	{
+		cout << "Please, choose the variant 'a' or 'b':" << endl;
+		cin >> choose;
+	} 
+	switch (choose)
+	{
+	case 'a':
+		a = 2;
+		b = 3;
+		h = 0.1;
+		cout << "Enter the value between " << a << " and " << b << ":" << endl;
+		cin >> x;
+		while (x < a || x > b)
+		{
+			cout << "Please, enter the valid value:" << endl;
+			cin >> x;
+		}
+		
+		for (x; x <= b + 0.001; x += h)
+		{
+			if (x < 2.3)
+			{
+				y = cos(x) + tan(x);
+			}
+			else if (x < 2.7)
+			{
+				y = 1 / tan(x) + sin(x);
+			}
+			else
+			{
+				y = pow(x*log(x), 3);
+			}
+			cout << "y(" << x << ") = " << y << endl;
+		}
+			
+			break;
+		
+	case 'b':
+		a = 0.1;
+		b = 1;
+		h = 0.1;
+		d = 0.001;
+		int k = 0;
+		double sum = 0;
+		cout << "Enter the value between " << a << " and " << b << ":" << endl;
+		cin >> x;
+		while (x < a || x > b)
+		{
+			cout << "Please, enter the valid value:" << endl;
+			cin >> x;
+		}
+		
+		for (x; x <= b + 0.001; x += h)
+		{
+
+			do
+			{
+				y = ((x / (2 * k - 1)*(2 * k + 3))*cos(2 * k + 1));
+				k++;
+				sum += y;
+
+			} while (fabs(y) >= d);
+			cout << "x = " << x << " ; y = " << y << "; sum = " << sum << endl;
+		}
+			break;
+
+		}
+	}
+
+
---
+++ lab3.cpp
@@ -0,0 +1,136 @@
+// lab3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#define n 5
+using namespace std;
+
+void sorting(int a[n][n]);//функція сортування
+void calc(int a[n][n]);//функція обчислень
+
+int main()
+{
+	int i, j, k;
+
+	int a[n][n];
+
+	for (i = 0; i<n; i++)
+
+	{
+
+		for (j = 0; j<n; j++)
+
+		{
+
+			cout << "a[" << i + 1 << "][" << j + 1 << "]=";
+			cin >> a[i][j];
+
+		}
+
+	}
+
+	cout << "the original massive:" << endl;
+
+	for (i = 0; i<n; i++)
+
+	{
+
+		for (j = 0; j < n; j++)
+		{
+			cout << "     " << a[i][j];
+		}
+		cout << endl;
+
+	}
+
+	sorting(a);
+
+	cout << "the sorted massive:" << endl;
+
+	for (i = 0; i<n; i++)
+
+	{
+
+		for (j = 0; j<n; j++)
+
+		{
+
+			cout << "     " << a[i][j];
+
+		}
+
+		cout << endl;
+
+	}
+
+	calc(a);
+
+}
+
+void sorting(int a[n][n])
+
+{
+
+	int i, j, k, c;
+
+	for (j = 0; j<n; j++)
+
+	{
+
+		for (k = n - 1; k >= 0; k--)
+
+		{
+
+			for (i = 0; i<k; i++)
+
+			{
+
+				if (a[i][j]<a[i + 1][j])
+
+				{
+
+					c = a[i][j];
+
+					a[i][j] = a[i + 1][j];
+
+					a[i + 1][j] = c;
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
+
+void calc(int a[n][n])
+
+{
+
+	int i, j;
+
+	double f;
+	double F = 0;
+
+	for (i = 1; i<n; i++)
+
+	{
+
+		f = 1;
+
+		for (j = n - i; j<n; j++)
+		{	
+
+			f = f * a[i][j];
+
+		}
+		F = F + f / (n - 1);
+		cout << "f(" << i + 1 << ")=" << f << endl;
+	}
+
+	cout << "F=" << F << endl;
+
+}
